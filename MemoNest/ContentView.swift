c04db22853750d0ce1a2be6e5f3bb6d652f5652a
fatimabk1 <fatima.kahbi@gmail.com>
2024-02-14 11:40:01 -0500

    Initial Commit

---
+++ MemoNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoNest
+//
+//  Created by Fatima Kahbi on 2/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoNest/MemoNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoNestApp.swift
+//  MemoNest
+//
+//  Created by Fatima Kahbi on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
