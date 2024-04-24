0a0b0dbb26cf2a3ea303341949d20dfbe56f4e78
fatimabk1 <fatima.kahbi@gmail.com>
2024-04-24 15:24:00 -0400

    Remove mock data from contentview (left behind from testing)

--- MemoNest/Views/ContentView.swift
+++ MemoNest/Views/ContentView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-//    let database = RealmDataManager()
-
-    let database = MockDataManager(folders: MockDataManager.sampleFolders, files: [Item(name: "Philosophy Lecture #4, The Self", date: Date(), type: .recording, audioInfo: AudioMetaData(duration: 123, recordingURLFileName: "www.sample.com")), Item(name: "PHilosophy Review nOtes", date: Date(), type: .recording, audioInfo: AudioMetaData(duration: 123, recordingURLFileName: "www.sample.com"))])
+    let database = RealmDataManager()
     
     var body: some View {
         NavigationStack {
